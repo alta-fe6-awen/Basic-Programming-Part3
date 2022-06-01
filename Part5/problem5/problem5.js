6be0261f3be461a0a0acd9009fe8ae644f92bec4
Awen <awendml@gmail.com>
2022-06-01 12:22:31 +0800

    Problem5 ditambahkan

--- Part5/problem5/problem5.js
+++ Part5/problem5/problem5.js
@@ -1,5 +1,6 @@
 function removeDuplicates(array) {
-	return Array.from(new Set(array));
+	let arrayLength = Array.from(new Set(array));
+	return arrayLength.length;
 }
 
 console.log(removeDuplicates([2, 3, 3, 3, 6, 9, 9])); // 4
@@ -7,7 +8,3 @@ console.log(removeDuplicates([2, 3, 4, 5, 6, 9, 9])); // 6
 console.log(removeDuplicates([2, 2, 2, 11])); // 2
 console.log(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 4])); // 4
 console.log(removeDuplicates([1, 2, 3, 11, 11])); // 4
-
-const array2 = [3,2,3,2,];
-
-console.log( Array.from(new Set(array2)));;
